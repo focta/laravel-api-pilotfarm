8f5b25e3f15bcdf4c84eeda92d999dc9052bcba6
focta <focke.wulf7234@gmail.com>
2022-11-29 00:19:54 +0900

    TailwindcCssの使い方を利用するための準備

--- src/resources/views/layouts/guest.blade.php
+++ src/resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }} defer"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
     </head>
     <body>
