12aead40168fa4a3ce49c02cd58b815f2e33328c
focta <focke.wulf7234@gmail.com>
2022-11-30 17:12:09 +0900

    クラスベースのコンポーネントを実装(ちょっと使いにくい)

--- src/resources/views/tests/component-test2.blade.php
+++ src/resources/views/tests/component-test2.blade.php
@@ -1,1 +1,6 @@
+<x-tests.app>
 コンポーネントテスト2
+    <x-test-class-base classBaseMessage="クラスメッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスメッセージです" defaultMessage="初期値から変更しています"/>
+</x-tests.app>
